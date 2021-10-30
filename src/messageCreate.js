cde182a594e7f615d4b90e994d127507286fda49
DeepPG <92751452+DeepPG@users.noreply.github.com>
2021-10-30 04:35:07 -0700

    Update messageCreate.js

--- src/messageCreate.js
+++ src/messageCreate.js
@@ -1,23 +1,25 @@
-﻿const axios = require('axios').default;
+const axios = require('axios').default;
 
 
 
 
 module.exports = {
-    message: async function message({content, channelID, token, messageEmbed:{title}}){
+    message: async function message({content, channelID, token, messageEmbed:{title, description}}){
         if(!test) throw new Error('cannot send empty message')
         if(!token) throw new Error('no token provided')
         if(!channelID) throw new Error('no channelID provided')
         axios.post(`https://discord.com/api/v8/channels/${channelID}/messages`,{
             "content": content,
             "embeds": [{
+                "description": description,
                 "title": title
             }]
         },  {
             headers: {
-                Authorization: `Bot ${token}`,            
+                Authorization: `Bot ${token}`,        
             }
         })
     }
+
 };
 
